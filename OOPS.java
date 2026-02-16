83bb3d311781dd2f8a7343fa26527923b5dcbe95
Namamie Nayanie <nayanienamamie10@gmail.com>
2026-02-16 11:07:46 +0530

    Add files via upload

---
+++ OOPS.java
@@ -0,0 +1,14 @@
+public class OOPS{
+	public static void main(String args[])
+	{
+		System.out.println("   ***   " + " " + "   ***   " + " " + "******   " + " " + "  *****  ");
+        System.out.println(" **   **" + " " +  " **   **" + " " + " **    ** " + " " + " **      ");
+		System.out.println("**     **" + " " + "**     **" + " " + "**     **" + " " + "**       ");
+		System.out.println("**     **" + " " + "**     **" + " " + "**    ** " + " " + " **      ");
+		System.out.println("**     **" + " " + "**     **" + " " + "******   " + " " + "   ***   ");
+		System.out.println("**     **" + " " + "**     **" + " " + "**       " + " " + "      ** ");
+		System.out.println("**     **" + " " + "**     **" + " " + "**       " + " " + "       **");
+		System.out.println(" **   **" + " " +  " **   **" + " " + "**       " + " " + "       **  ");
+		System.out.println("   ***  " + " " + "    ***  " + " " + "**       " + " " + "  *****   ");
+		
+}}
